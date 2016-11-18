de8134d61a204cf32ca56c296f58b625a101c6cf
Dizner <xiaxingkong@sina.com>
2016-11-18 16:41:40 +0800

    减肥修改

--- app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
+++ app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
@@ -1,26 +1,28 @@
 package com.boohee.boohee.fragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.boohee.boohee.R;
 
 /**
  * Created by Dizner on 2016/11/18.
  */
 
 public class LoseWeightFragment extends Fragment {
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView view=new TextView(getContext());
-        view.setText("减肥");
-        view.setTextColor(Color.YELLOW);
-        view.setTextSize(20);
+        if (view == null) {
+            view = inflater.inflate(R.layout.loseweight_fragment, container, false);
+        }
+
         return view;
     }
 }
