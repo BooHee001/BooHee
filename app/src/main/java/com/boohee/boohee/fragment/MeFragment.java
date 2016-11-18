194853a5bbb2a6bbdc9bc3ab667ebadfcfcdaf5f
柴鹏飞 <545558178@qq.com>
2016-11-18 17:53:10 +0800

    没啥内容

--- app/src/main/java/com/boohee/boohee/fragment/MeFragment.java
+++ app/src/main/java/com/boohee/boohee/fragment/MeFragment.java
@@ -6,18 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.boohee.boohee.R;
+
+import butterknife.OnClick;
 
 /**
  * Created by Dizner on 2016/11/18.
  */
 
 public class MeFragment extends Fragment {
+
+
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.me_fragment,container,false);
         return view;
+
+
+
     }
+
+    @OnClick(R.id.me_personaldata)
+    public void me_personaldata(View view){
+        Toast.makeText(getActivity(), "123", Toast.LENGTH_SHORT).show();
+    }
+
 }
