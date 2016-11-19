4942e0a479597360985389d17aacfe57d6357f1b
Dizner <xiaxingkong@sina.com>
2016-11-19 23:38:11 +0800

    MVP模式View层主页，ViewPAger添加

---
+++ app/src/main/java/com/boohee/boohee/model/ApiService.java
@@ -0,0 +1,11 @@
+package com.boohee.boohee.model;
+
+/**
+ * Created by Dizner on 2016/11/19.
+ */
+
+public interface ApiService {
+
+//    @GET("service/getIpInfo.php")
+//    Call<GetIpInfoResponse> getIpInfo(@Query("ip") String ip);
+}
