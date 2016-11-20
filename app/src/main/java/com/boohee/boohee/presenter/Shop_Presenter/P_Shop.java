14bc56c87edbd2a5b047a535fdd1929c7c4f535b
张辽 <2587685778@qq.com>
2016-11-21 01:23:15 +0800

    商品首页p层接口和实现类

---
+++ app/src/main/java/com/boohee/boohee/presenter/Shop_Presenter/P_Shop.java
@@ -0,0 +1,11 @@
+package com.boohee.boohee.presenter.Shop_Presenter;
+
+import com.boohee.boohee.Bean.Shop_Bean.Shop_Goods;
+
+/**
+ * Created by as on 2016/11/20.
+ */
+
+public interface P_Shop {
+    void setShopData(Shop_Goods shop_goods);
+}
