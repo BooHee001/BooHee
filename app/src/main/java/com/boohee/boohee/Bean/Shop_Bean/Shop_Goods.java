cda77db6399dab7180deac6c53778f928925dc5c
张辽 <2587685778@qq.com>
2016-11-21 01:20:08 +0800

    第一次修改后的商品首页Bean

--- app/src/main/java/com/boohee/boohee/Bean/Shop_Bean/Shop_Goods.java
+++ app/src/main/java/com/boohee/boohee/Bean/Shop_Bean/Shop_Goods.java
@@ -83,6 +83,7 @@ public class Shop_Goods {
              * exhibit_type : goods
              */
 
+
             private int id;
             private String exhibit;
             private String page_title;
@@ -90,6 +91,13 @@ public class Shop_Goods {
             private int default_photo_height;
             private int default_photo_width;
             private String exhibit_type;
+            private String title;
+            private String base_price;
+            private String market_price;
+            private String category_name;
+            private String thumb_photo_url;
+            private String big_photo_url;
+            private String state;
 
             public int getId() {
                 return id;
@@ -145,6 +153,82 @@ public class Shop_Goods {
 
             public void setExhibit_type(String exhibit_type) {
                 this.exhibit_type = exhibit_type;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getBase_price() {
+                return base_price;
+            }
+
+            public void setBase_price(String base_price) {
+                this.base_price = base_price;
+            }
+
+            public String getMarket_price() {
+                return market_price;
+            }
+
+            public void setMarket_price(String market_price) {
+                this.market_price = market_price;
+            }
+
+            public String getCategory_name() {
+                return category_name;
+            }
+
+            public void setCategory_name(String category_name) {
+                this.category_name = category_name;
+            }
+
+            public String getThumb_photo_url() {
+                return thumb_photo_url;
+            }
+
+            public void setThumb_photo_url(String thumb_photo_url) {
+                this.thumb_photo_url = thumb_photo_url;
+            }
+
+            public String getBig_photo_url() {
+                return big_photo_url;
+            }
+
+            public void setBig_photo_url(String big_photo_url) {
+                this.big_photo_url = big_photo_url;
+            }
+
+            public String getState() {
+                return state;
+            }
+
+            public void setState(String state) {
+                this.state = state;
+            }
+
+            public ListBean() {
+            }
+
+            public ListBean(int id, String exhibit, String page_title, String default_photo_url, int default_photo_height, int default_photo_width, String exhibit_type, String title, String base_price, String market_price, String category_name, String thumb_photo_url, String big_photo_url, String state) {
+                this.id = id;
+                this.exhibit = exhibit;
+                this.page_title = page_title;
+                this.default_photo_url = default_photo_url;
+                this.default_photo_height = default_photo_height;
+                this.default_photo_width = default_photo_width;
+                this.exhibit_type = exhibit_type;
+                this.title = title;
+                this.base_price = base_price;
+                this.market_price = market_price;
+                this.category_name = category_name;
+                this.thumb_photo_url = thumb_photo_url;
+                this.big_photo_url = big_photo_url;
+                this.state = state;
             }
         }
     }
