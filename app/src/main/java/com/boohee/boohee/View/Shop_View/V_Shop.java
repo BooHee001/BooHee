5bd81a3ff51668ba6d60fc8a9609a79cfc9fac79
张辽 <2587685778@qq.com>
2016-11-21 01:24:11 +0800

    v层自定义gridView和v层实现类

---
+++ app/src/main/java/com/boohee/boohee/View/Shop_View/V_Shop.java
@@ -0,0 +1,11 @@
+package com.boohee.boohee.View.Shop_View;
+
+import com.boohee.boohee.Bean.Shop_Bean.Shop_Goods;
+
+/**
+ * Created by as on 2016/11/20.
+ */
+
+public interface V_Shop {
+    void setShopData(Shop_Goods shop_goods);
+}
