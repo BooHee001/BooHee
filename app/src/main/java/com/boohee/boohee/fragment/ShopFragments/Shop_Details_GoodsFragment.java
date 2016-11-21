1a56b6b7974a800792d8b130b9ccddfd65670f99
张辽 <2587685778@qq.com>
2016-11-22 00:43:02 +0800

    商城修改

---
+++ app/src/main/java/com/boohee/boohee/fragment/ShopFragments/Shop_Details_GoodsFragment.java
@@ -0,0 +1,23 @@
+package com.boohee.boohee.fragment.ShopFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by as on 2016/11/21.
+ */
+
+public class Shop_Details_GoodsFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView tv = new TextView(getContext());
+        tv.setText("商品");
+        return tv;
+    }
+}
