2561d9326aa5d0f0a7201633342feb402a4a6eef
张辽 <2587685778@qq.com>
2016-11-22 00:43:12 +0800

    商城修改

---
+++ app/src/main/java/com/boohee/boohee/Bean/Shop_Bean/Shop_Goods_Item.java
@@ -0,0 +1,286 @@
+package com.boohee.boohee.Bean.Shop_Bean;
+
+import java.util.List;
+
+/**
+ * Created by as on 2016/11/21.
+ */
+
+public class Shop_Goods_Item {
+
+    /**
+     * id : 82
+     * name : 所有商品-运动装备
+     * label_type : topic
+     * description : APP-商品4-运动装备
+     * labels : [{"id":100,"name":"微店-智能硬件-所有商品","label_type":"category","description":"WX-商品3-智能硬件"},{"id":47,"name":"茶类","label_type":"category","description":"加餐/冲饮的子分类"},{"id":51,"name":"蔬果类","label_type":"category","description":"冲饮的子分类"},{"id":67,"name":"超模25网站商品页","label_type":"category","description":"超模25网站商品页"},{"id":68,"name":"超模25网站首页","label_type":"category","description":"超模25网站首页"},{"id":59,"name":"尊享型","label_type":"category","description":"代餐的子分类"},{"id":61,"name":"酸奶分类","label_type":"category","description":"酸奶的分类"}]
+     * banner_showcases : []
+     * goods : [{"id":972,"slug":"joinfit_pad_bh_160823","type":"SpecGoods","title":"在家运动不能少｜Joinfit 专业TPE 双面瑜伽垫","base_price":"186.0","market_price":"260.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_pad_8_bh/thumb_g_v2.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_pad_8_bh/thumb_g_v2.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":1044,"slug":"joinfit_jsd_10mm_20161011","type":"SpecGoods","title":"健身入门款｜JOINFIT 加宽NBR健身垫（3色可选）","base_price":"78.0","market_price":"98.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_jsd_10mm/thumb.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_jsd_10mm/thumb.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":971,"slug":"joinfit_ball_bh_160823","type":"SpecGoods","title":"瘦腹瘦腿必备｜Joinfit 防爆瑜伽球","base_price":"99.0","market_price":"128.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_ball_65_bh/thumb_b_v1.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_ball_65_bh/thumb_b_v1.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":969,"slug":"joinfit_roller_bh_160823","type":"SpecGoods","title":"滚出好身材｜Joinfit 浮点泡沫轴 45cm 2色可选","base_price":"68.0","market_price":"138.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_roller_bh/thumb_p_v2.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_roller_bh/thumb_p_v2.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":970,"slug":"joinfit_band_bh_160823","type":"SpecGoods","title":"细胳膊细腿｜Joinfit 弹力带 初中级可选","base_price":"28.0","market_price":"42.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_band_bh/slider/thumb_v1.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/joinfit_band_bh/slider/thumb_v1.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":922,"slug":"Rawness_weightplate_necklace","type":"SpecGoods","title":"雕刻般的身材｜Rawness 杠铃片项链","base_price":"339.0","market_price":"535.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_weightplate_necklace.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_weightplate_necklace.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":895,"slug":"Rawness_couples_160802","type":"SpecGoods","title":"爱TA就和TA一起举铁｜Rawness 情侣哑铃项链","base_price":"430.0","market_price":"1198.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_db_bright_couples.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_db_bright_couples.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":923,"slug":"Rawness_dumbbell_necklace","type":"SpecGoods","title":"脖子上的信念｜Rawness 哑铃项链","base_price":"430.0","market_price":"569.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_dumbbell_necklace_v1.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/list/rawness_dumbbell_necklace_v1.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":1,"category_name":"食品","flag_url":null,"state":"on_sale"},{"id":1083,"slug":"p_tm25_zp_bottle01_480_161101","type":"PackageGoods","title":"欧美婴幼用品指定材质 | 超模25定制Tritan摇摇杯 480ml","base_price":"48.0","market_price":"68.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/p_tm25_zp_bottle/slider/s1.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/p_tm25_zp_bottle/slider/s1.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"},{"id":1122,"slug":"lifestraw_650ml_161108","type":"SpecGoods","title":"薄荷首发 贝爷推荐｜瑞士LifeStraw®Go生命水壶Plus  650ml","base_price":"399.0","market_price":"489.0","thumb_photo_url":"http://up.boohee.cn/house/u/shop/lifestraw/thumb.jpg?imageView/2/w/96/h/96","big_photo_url":"http://up.boohee.cn/house/u/shop/lifestraw/thumb.jpg?imageView/2/w/608/h/344","a_photo_url":null,"b_photo_url":null,"c_photo_url":null,"category_id":2,"category_name":"装备","flag_url":null,"state":"on_sale"}]
+     */
+
+    private int id;
+    private String name;
+    private String label_type;
+    private String description;
+    private List<LabelsBean> labels;
+    private List<?> banner_showcases;
+    private List<GoodsBean> goods;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLabel_type() {
+        return label_type;
+    }
+
+    public void setLabel_type(String label_type) {
+        this.label_type = label_type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<LabelsBean> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<LabelsBean> labels) {
+        this.labels = labels;
+    }
+
+    public List<?> getBanner_showcases() {
+        return banner_showcases;
+    }
+
+    public void setBanner_showcases(List<?> banner_showcases) {
+        this.banner_showcases = banner_showcases;
+    }
+
+    public List<GoodsBean> getGoods() {
+        return goods;
+    }
+
+    public void setGoods(List<GoodsBean> goods) {
+        this.goods = goods;
+    }
+
+    public static class LabelsBean {
+        /**
+         * id : 100
+         * name : 微店-智能硬件-所有商品
+         * label_type : category
+         * description : WX-商品3-智能硬件
+         */
+
+        private int id;
+        private String name;
+        private String label_type;
+        private String description;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLabel_type() {
+            return label_type;
+        }
+
+        public void setLabel_type(String label_type) {
+            this.label_type = label_type;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+    public static class GoodsBean {
+        /**
+         * id : 972
+         * slug : joinfit_pad_bh_160823
+         * type : SpecGoods
+         * title : 在家运动不能少｜Joinfit 专业TPE 双面瑜伽垫
+         * base_price : 186.0
+         * market_price : 260.0
+         * thumb_photo_url : http://up.boohee.cn/house/u/shop/joinfit_pad_8_bh/thumb_g_v2.jpg?imageView/2/w/96/h/96
+         * big_photo_url : http://up.boohee.cn/house/u/shop/joinfit_pad_8_bh/thumb_g_v2.jpg?imageView/2/w/608/h/344
+         * a_photo_url : null
+         * b_photo_url : null
+         * c_photo_url : null
+         * category_id : 2
+         * category_name : 装备
+         * flag_url : null
+         * state : on_sale
+         */
+
+        private int id;
+        private String slug;
+        private String type;
+        private String title;
+        private String base_price;
+        private String market_price;
+        private String thumb_photo_url;
+        private String big_photo_url;
+        private Object a_photo_url;
+        private Object b_photo_url;
+        private Object c_photo_url;
+        private int category_id;
+        private String category_name;
+        private Object flag_url;
+        private String state;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getSlug() {
+            return slug;
+        }
+
+        public void setSlug(String slug) {
+            this.slug = slug;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getBase_price() {
+            return base_price;
+        }
+
+        public void setBase_price(String base_price) {
+            this.base_price = base_price;
+        }
+
+        public String getMarket_price() {
+            return market_price;
+        }
+
+        public void setMarket_price(String market_price) {
+            this.market_price = market_price;
+        }
+
+        public String getThumb_photo_url() {
+            return thumb_photo_url;
+        }
+
+        public void setThumb_photo_url(String thumb_photo_url) {
+            this.thumb_photo_url = thumb_photo_url;
+        }
+
+        public String getBig_photo_url() {
+            return big_photo_url;
+        }
+
+        public void setBig_photo_url(String big_photo_url) {
+            this.big_photo_url = big_photo_url;
+        }
+
+        public Object getA_photo_url() {
+            return a_photo_url;
+        }
+
+        public void setA_photo_url(Object a_photo_url) {
+            this.a_photo_url = a_photo_url;
+        }
+
+        public Object getB_photo_url() {
+            return b_photo_url;
+        }
+
+        public void setB_photo_url(Object b_photo_url) {
+            this.b_photo_url = b_photo_url;
+        }
+
+        public Object getC_photo_url() {
+            return c_photo_url;
+        }
+
+        public void setC_photo_url(Object c_photo_url) {
+            this.c_photo_url = c_photo_url;
+        }
+
+        public int getCategory_id() {
+            return category_id;
+        }
+
+        public void setCategory_id(int category_id) {
+            this.category_id = category_id;
+        }
+
+        public String getCategory_name() {
+            return category_name;
+        }
+
+        public void setCategory_name(String category_name) {
+            this.category_name = category_name;
+        }
+
+        public Object getFlag_url() {
+            return flag_url;
+        }
+
+        public void setFlag_url(Object flag_url) {
+            this.flag_url = flag_url;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+    }
+}
