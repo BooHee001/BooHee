922d1e54802f4938d548ef4fd754c3857f67eb07
方鹏 <949180084@qq.com>
2016-11-23 00:40:37 +0800

    下拉刷新

--- app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
+++ app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
@@ -1,8 +1,12 @@
 package com.boohee.boohee.fragment;
 
 import android.animation.ObjectAnimator;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
@@ -12,10 +16,14 @@ import android.support.v4.widget.NestedScrollView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.boohee.boohee.R;
+import com.bumptech.glide.Glide;
 import com.github.lzyzsd.circleprogress.ArcProgress;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Dizner on 2016/11/18.
@@ -31,6 +39,10 @@ public class LoseWeightFragment extends Fragment {
     private PagerAdapter adapter;
     private int width;
     private int hight;
+
+    private RelativeLayout home_welcome_imgs;
+    private Bitmap bitmap=null;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
     @Override
@@ -38,6 +50,7 @@ public class LoseWeightFragment extends Fragment {
         if (view == null) {
             view = inflater.inflate(R.layout.loseweight_fragment, container, false);
             progress= (ArcProgress) view.findViewById(R.id.weight_progress);
+            home_welcome_imgs= (RelativeLayout) view.findViewById(R.id.home_welcome_imgs);
             textView= (TextView) view.findViewById(R.id.textView);
 
             textView.setOnClickListener(new View.OnClickListener() {
@@ -66,5 +79,34 @@ public class LoseWeightFragment extends Fragment {
         animator.setDuration(duration);
         animator.start();
     }
+    public void setWelcomeImgs(final String url){
+            new Thread(){
+                @Override
+                public void run() {
 
+                    try {
+                        bitmap= Glide.with(LoseWeightFragment.this)
+                                .load(url)
+                                .asBitmap()
+                                .centerCrop()
+                                .into(width, home_welcome_imgs.getHeight())
+                                .get();
+                        handler.sendEmptyMessage(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+
+
+    }
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            home_welcome_imgs.setBackground(new BitmapDrawable(bitmap));
+        }
+    };
 }
