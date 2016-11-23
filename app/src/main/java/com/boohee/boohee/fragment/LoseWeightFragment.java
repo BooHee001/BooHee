3700281f88107234b0fc14ebe02bea64b75738c4
方鹏 <949180084@qq.com>
2016-11-23 22:44:35 +0800

    首页图片二次修改

--- app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
+++ app/src/main/java/com/boohee/boohee/fragment/LoseWeightFragment.java
@@ -1,5 +1,6 @@
 package com.boohee.boohee.fragment;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -61,14 +62,6 @@ public class LoseWeightFragment extends Fragment {
                         i-=1;
                         setAnim(300);
 
-                        decorView.setSystemUiVisibility(
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION//隐藏状态栏的布局
-                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN//内容布局填满屏幕
-                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//隐藏状态栏
-                                        | View.SYSTEM_UI_FLAG_FULLSCREEN//内容全屏展示
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        );
                     }
                 }
             });
@@ -88,10 +81,37 @@ public class LoseWeightFragment extends Fragment {
         ObjectAnimator animator=ObjectAnimator.ofFloat(home_showPhoto,"translationY",-hight,0);
         animator.setDuration(duration);
 
+
+
+       animator.addListener(new Animator.AnimatorListener() {
+           @Override
+           public void onAnimationStart(Animator animation) {
+
+           }
+
+           @Override
+           public void onAnimationEnd(Animator animation) {
+               decorView.setSystemUiVisibility(
+                       View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION//隐藏状态栏的布局
+                               | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN//内容布局填满屏幕
+//                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//隐藏状态栏
+                               | View.SYSTEM_UI_FLAG_FULLSCREEN//内容全屏展示
+                               | View.SYSTEM_UI_FLAG_IMMERSIVE
+               );
+           }
+
+           @Override
+           public void onAnimationCancel(Animator animation) {
+
+           }
+
+           @Override
+           public void onAnimationRepeat(Animator animation) {
+
+           }
+       });
         animator.start();
-
-
-
 
     }
     public void setWelcomeImgs(final String url){
