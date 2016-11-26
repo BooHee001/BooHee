82a155a552a143a2cfbd68911698a26339943a07
张辽 <2587685778@qq.com>
2016-11-26 09:04:53 +0800

    P层2016-11-26

---
+++ app/src/main/java/com/boohee/boohee/presenter/Shop_Presenter/P_ShowGoodsCar.java
@@ -0,0 +1,13 @@
+package com.boohee.boohee.presenter.Shop_Presenter;
+
+import com.boohee.boohee.Bean.Shop_Bean.GoodsCarBean;
+
+import java.util.List;
+
+/**
+ * Created by as on 2016/11/25.
+ */
+public interface P_ShowGoodsCar {
+    void getData(List<GoodsCarBean> goodsCarBeanList);
+
+}
