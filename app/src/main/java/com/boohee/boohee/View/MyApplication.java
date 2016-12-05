e1a2a8fad74ce0e797c1a4e18c544852f527db64
张辽 <2587685778@qq.com>
2016-12-06 00:39:26 +0800

    zzzzzzzzzzhki

--- app/src/main/java/com/boohee/boohee/View/MyApplication.java
+++ app/src/main/java/com/boohee/boohee/View/MyApplication.java
@@ -10,7 +10,7 @@ import org.xutils.x;
 
 public class MyApplication extends Application {
     public static String MyIp = "10.35.162.135";
-    public static int userno = 0;
+    public static int userno = 3321;
     @Override
     public void onCreate() {
         super.onCreate();
